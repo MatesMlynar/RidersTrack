58e2d1a09ded9c190a000532290425de02874195
Matěj Mlynář <mata.mlynar@seznam.cz>
2023-11-06 20:51:11 +0100

    [frontend] tracking page touch ups

--- frontend/lib/views/tracking_page.dart
+++ frontend/lib/views/tracking_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:frontend/views/layout/layout_page.dart';
 
 class TrackingPage extends StatefulWidget {
   const TrackingPage({super.key, required this.motorcycleId});
@@ -49,62 +50,66 @@ class _TrackingPageState extends State<TrackingPage> {
 
     String formattedTime = formatTime(seconds);
 
-    return  Scaffold(
-      backgroundColor: const Color.fromARGB(255, 20, 24, 27),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(15, 70, 15, 70),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            const Column(
+    return  WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 20, 24, 27),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 70, 15, 70),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("RECORDING YOUR RIDE...", style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
-                Text("Make sure your phone has enabled gps. Enjoy your ride!", style: TextStyle(color: Colors.white, fontSize: 15.0)),
-              ],
-            ),
-            Column(
-              children: [
-                Text(formattedTime, style: const TextStyle(color: Colors.white, fontSize: 70.0, fontWeight: FontWeight.bold)),
-                const Text('duration', style: TextStyle(color: Colors.white, fontSize: 20.0)),
-              ],
-            ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              const Column(
                 children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          isRunning = !isRunning;
-                        });},
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(30),
-                        backgroundColor: Colors.red, // <-- Button color
-                        foregroundColor: Colors.grey, // <-- Splash color
-                      ),
-                      child: isRunning ? const Icon(Icons.pause, color: Colors.white) : const Icon(Icons.play_arrow, color: Colors.white)
-                  ),
-                  !isRunning ? const SizedBox(width: 20) : const SizedBox(width: 0),
-                  !isRunning ? ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          print('todo create a command that will create new ride and call it here');
-                          //todo create a command that will create new ride and call it from here
-                        });},
-                      style: ElevatedButton.styleFrom(
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(30),
-                        backgroundColor: Colors.red, // <-- Button color
-                        foregroundColor: Colors.grey, // <-- Splash color
-                      ),
-                      child: const Icon(Icons.stop, color: Colors.white)
-                  ) : const SizedBox(width: 0),
+                  Text("RECORDING YOUR RIDE...", style: TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold)),
+                  Text("Make sure your phone has enabled gps. Enjoy your ride!", style: TextStyle(color: Colors.white, fontSize: 15.0)),
                 ],
-              )
+              ),
+              Column(
+                children: [
+                  Text(formattedTime, style: const TextStyle(color: Colors.white, fontSize: 70.0, fontWeight: FontWeight.bold)),
+                  const Text('duration', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                ],
+              ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isRunning = !isRunning;
+                          });},
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(30),
+                          backgroundColor: Colors.red, // <-- Button color
+                          foregroundColor: Colors.grey, // <-- Splash color
+                        ),
+                        child: isRunning ? const Icon(Icons.pause, color: Colors.white) : const Icon(Icons.play_arrow, color: Colors.white)
+                    ),
+                    !isRunning ? const SizedBox(width: 20) : const SizedBox(width: 0),
+                    !isRunning ? ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            print('todo create a command that will create new ride and call it here');
+                            //todo create a command that will create new ride and call it from here
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const LayoutPage()));
+                          });},
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(30),
+                          backgroundColor: Colors.red, // <-- Button color
+                          foregroundColor: Colors.grey, // <-- Splash color
+                        ),
+                        child: const Icon(Icons.stop, color: Colors.white)
+                    ) : const SizedBox(width: 0),
+                  ],
+                )
 
 
-          ],),
+            ],),
+          ),
         ),
       ),
     );
