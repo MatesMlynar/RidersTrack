a4c2899f39a6046e4b90d941d28bfecbcaa31e5a
Matěj Mlynář <mata.mlynar@seznam.cz>
2023-11-08 08:49:32 +0100

    [frontend] init workmanager implemented

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -19,13 +19,13 @@ import 'package:workmanager/workmanager.dart';
 
 import 'models/fuel_record_model.dart';
 
-@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    print("Native called background task: $task"); //simpleTask will be emitted here.
-    return Future.value(true);
-  });
-}
+ @pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+ void callbackDispatcher() {
+   Workmanager().executeTask((task, inputData) {
+     print("Native called background task: $task"); //simpleTask will be emitted here.
+     return Future.value(true);
+   });
+ }
 
 
 Future main() async {
