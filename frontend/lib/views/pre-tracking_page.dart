4818356fa9fde87d4521c228988df4d042fbb044
Matěj Mlynář <mata.mlynar@seznam.cz>
2023-11-16 21:46:20 +0100

    [frontend] pre-tracking page edited

--- frontend/lib/views/pre-tracking_page.dart
+++ frontend/lib/views/pre-tracking_page.dart
@@ -33,7 +33,7 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
       return;
     }
 
-    if(_isMounted){
+    if (_isMounted) {
       setState(() {
         isMotoFetching = false;
         if (result['data'] != null && result['data'].isNotEmpty) {
@@ -45,9 +45,7 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
   }
 
 
-
-  void checkPermission() async{
-
+  void checkPermission() async {
     //check the permissions and if valid -> route
     bool serviceEnabled;
     LocationPermission permission;
@@ -58,7 +56,9 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
       // Location services are not enabled don't continue
       // accessing the position and request users of the
       // App to enable the location services.
-      SnackBarService.showSnackBar(content: 'Location services are disabled. You won\'t be able to track your ride', color: Colors.red);
+      SnackBarService.showSnackBar(
+          content: 'Location services are disabled. You won\'t be able to track your ride',
+          color: Colors.red);
 
       return Future.error('Location services are disabled.');
     }
@@ -72,14 +72,18 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
         // Android's shouldShowRequestPermissionRationale
         // returned true. According to Android guidelines
         // your App should show an explanatory UI now.
-        SnackBarService.showSnackBar(content: 'You won\'t be able to record your ride due deny location permission', color: Colors.red);
+        SnackBarService.showSnackBar(
+            content: 'You won\'t be able to record your ride due deny location permission',
+            color: Colors.red);
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
-      SnackBarService.showSnackBar(content: 'Location permissions are permanently denied, we cannot request permissions', color: Colors.red);
+      SnackBarService.showSnackBar(
+          content: 'Location permissions are permanently denied, we cannot request permissions',
+          color: Colors.red);
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
@@ -87,8 +91,9 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
 
-    if(context.mounted){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => TrackingPage(motorcycleId: selectedMotorcycleId!)));
+    if (context.mounted) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+          TrackingPage(motorcycleId: selectedMotorcycleId!)));
     }
   }
 
@@ -124,98 +129,111 @@ class _PreTrackingPageState extends State<PreTrackingPage> {
           child: Padding(
             padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                 const AutoSizeText(
-                    "Start with choosing one of your motorcycles",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                    maxLines: 1),
-                const SizedBox(
-                  height: 20,
+                "Start with choosing one of your motorcycles",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                maxLines: 1),
+            const SizedBox(
+              height: 20,
+            ),
+            isMotoFetching
+                ? const CircularProgressIndicator()
+                : Column(
+              children: [
+                DropdownButtonFormField(
+                  value: selectedMotorcycleId,
+                  //onChange method
+                  onChanged: (val) {
+                    setState(() {
+                      selectedMotorcycleId = val as String;
+                    });
+                  },
+                  //items
+                  items: motorcycleIdsList
+                      .map((itemVal) =>
+                      DropdownMenuItem(
+                        value: itemVal['_id'],
+                        child: Text(itemVal['name']),
+                      ))
+                      .toList(),
+                  //style
+                  icon: const Icon(
+                    Icons.arrow_drop_down_circle,
+                    color: Colors.white,
+                  ),
+                  style:
+                  const TextStyle(color: Colors.white, fontSize: 16),
+                  dropdownColor: const Color.fromARGB(255, 20, 24, 27),
+                  decoration: const InputDecoration(
+                    labelText: "Select motorcycle *",
+                    labelStyle:
+                    TextStyle(color: Colors.white, fontSize: 16),
+                    prefixIcon:
+                    Icon(Icons.motorcycle, color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.grey, width: 1.5)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.white, width: 1.5)),
+                  ),
                 ),
-                isMotoFetching
-                    ? const CircularProgressIndicator()
-                    : Column(
-                      children: [
-                        DropdownButtonFormField(
-                            value: selectedMotorcycleId,
-                            //onChange method
-                            onChanged: (val) {
-                              setState(() {
-                                selectedMotorcycleId = val as String;
-                              });
-                            },
-                            //items
-                            items: motorcycleIdsList
-                                .map((itemVal) => DropdownMenuItem(
-                                      value: itemVal['_id'],
-                                      child: Text(itemVal['name']),
-                                    ))
-                                .toList(),
-                            //style
-                            icon: const Icon(
-                              Icons.arrow_drop_down_circle,
-                              color: Colors.white,
-                            ),
-                            style:
-                                const TextStyle(color: Colors.white, fontSize: 16),
-                            dropdownColor: const Color.fromARGB(255, 20, 24, 27),
-                            decoration: const InputDecoration(
-                              labelText: "Select motorcycle *",
-                              labelStyle:
-                                  TextStyle(color: Colors.white, fontSize: 16),
-                              prefixIcon:
-                                  Icon(Icons.motorcycle, color: Colors.white),
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide:
-                                      BorderSide(color: Colors.grey, width: 1.5)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide:
-                                      BorderSide(color: Colors.white, width: 1.5)),
-                            ),
-                          ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            GestureDetector(
-                              child: const Text("create motorcycle", style: TextStyle(color: Colors.grey, fontSize: 14, decoration: TextDecoration.underline, decorationColor: Colors.white),),
-                              onTap: () {
-                                print('todo add navigator that will open page where user can create new motorcycle');
-                                //todo add navigator that will open page where user can create new motorcycle
-                              },
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                const SizedBox(
-                  height: 40,
-                ),
-                OutlinedButton(
-                    onPressed: () {
-                      if (selectedMotorcycleId == null) {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Select motorcycle or create one!'), backgroundColor: Colors.red,));
-                        return;
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      child: const Text("create motorcycle", style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.white),),
+                      onTap: () {
+                        print(
+                            'todo add navigator that will open page where user can create new motorcycle');
+                        //todo add navigator that will open page where user can create new motorcycle
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            OutlinedButton(
+              onPressed: isMotoFetching ? null : () {
+                if (selectedMotorcycleId == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('Select motorcycle or create one!'),
+                    backgroundColor: Colors.red,));
+                  return;
+                }
+                //todo create method that will check the user location permission
+                checkPermission();
+              },
+              style: ButtonStyle(
+                  shape:
+                  MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12))),
+                  minimumSize: MaterialStateProperty.all<Size>(
+                      const Size(150, 50)),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.disabled)) {
+                        return Colors.grey;
                       }
-                      //todo create method that will check the user location permission
-                      checkPermission();
-                    },
-                    style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12))),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                            const Size(150, 50)),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color.fromARGB(255, 221, 28, 7))),
-                    child: Text(
-                      "Start tracking!",
-                      style: GoogleFonts.readexPro(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
-                    )),
+                      return const Color.fromARGB(255, 221, 28, 7);
+                    })),
+                  child: Text(
+                    "Start tracking!",
+                    style: GoogleFonts.readexPro(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
+                  )),
               ],
             ),
           ),
